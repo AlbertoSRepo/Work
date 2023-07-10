bfbc0a4de4edaefa9c98387c1779f1a784db3f30
Alberto Scandurra <AScandurra@matest.com>
2023-07-10 15:31:16 +0200

    Connessioni aperte multiple funzionanti

--- CommandClient.java
+++ CommandClient.java
@@ -2,25 +2,28 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.sql.Connection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CommandClient {
     private static final String SERVER_HOST = "localhost";
     private static final int SERVER_PORT = 4321;
 
-    private static final Map<String, Connection> openConnections = new HashMap<>();
+    private static final List<String> availableConnections = new ArrayList<>();
 
     public static void main(String[] args) {
-        try (Socket socket = new Socket(SERVER_HOST, SERVER_PORT);
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             Scanner scanner = new Scanner(System.in)) {
+        try (
+            Socket socket = new Socket(SERVER_HOST, SERVER_PORT);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner scanner = new Scanner(System.in)
+        ) {
+            // Avvia il thread per la lettura dei messaggi dal server
+            new Thread(new ServerListener(in)).start();
 
             while (true) {
-                System.out.println("Inserisci un comando ('connect', 'send', 'quit', 'close'): ");
+                System.out.println("Inserisci un comando ('connect', 'send', 'quit', 'close', 'list'): ");
                 String command = scanner.nextLine();
 
                 if ("connect".equalsIgnoreCase(command)) {
@@ -28,24 +31,23 @@ public class CommandClient {
                     String ipAddress = scanner.nextLine();
                     System.out.print("Inserisci la porta del server finale: ");
                     int port = scanner.nextInt();
-                    scanner.nextLine(); // Consume newline left-over
+                    scanner.nextLine(); // Consuma il newline rimanente
                     out.println(command);
                     out.println(ipAddress);
                     out.println(port);
+                } else if ("list".equalsIgnoreCase(command)) {
+                    out.println(command);
                 } else if ("send".equalsIgnoreCase(command)) {
-                    System.out.println("Connessioni aperte:");
-                    int index = 1;
-                    for (String connection : openConnections.keySet()) {
-                        System.out.println(index + ": " + connection);
-                        index++;
+                    System.out.println("Connessioni disponibili:");
+                    for (int i = 0; i < availableConnections.size(); i++) {
+                        System.out.println((i + 1) + ": " + availableConnections.get(i));
                     }
                     System.out.print("Seleziona l'indice della connessione: ");
                     int connectionIndex = scanner.nextInt();
-                    scanner.nextLine(); // Consume newline left-over
-                    
-                    if (connectionIndex > 0 && connectionIndex <= openConnections.size()) {
-                        String[] connections = openConnections.keySet().toArray(new String[0]);
-                        String selectedConnection = connections[connectionIndex - 1];
+                    scanner.nextLine(); // Consuma il newline rimanente
+
+                    if (connectionIndex > 0 && connectionIndex <= availableConnections.size()) {
+                        String selectedConnection = availableConnections.get(connectionIndex - 1);
                         System.out.print("Inserisci il messaggio: ");
                         String message = scanner.nextLine();
                         out.println(command);
@@ -55,19 +57,16 @@ public class CommandClient {
                         System.out.println("Indice non valido.");
                     }
                 } else if ("quit".equalsIgnoreCase(command)) {
-                    System.out.println("Connessioni aperte:");
-                    int index = 1;
-                    for (String connection : openConnections.keySet()) {
-                        System.out.println(index + ": " + connection);
-                        index++;
+                    System.out.println("Connessioni disponibili:");
+                    for (int i = 0; i < availableConnections.size(); i++) {
+                        System.out.println((i + 1) + ": " + availableConnections.get(i));
                     }
                     System.out.print("Seleziona l'indice della connessione: ");
                     int connectionIndex = scanner.nextInt();
-                    scanner.nextLine(); // Consume newline left-over
+                    scanner.nextLine(); // Consuma il newline rimanente
 
-                    if (connectionIndex > 0 && connectionIndex <= openConnections.size()) {
-                        String[] connections = openConnections.keySet().toArray(new String[0]);
-                        String selectedConnection = connections[connectionIndex - 1];
+                    if (connectionIndex > 0 && connectionIndex <= availableConnections.size()) {
+                        String selectedConnection = availableConnections.get(connectionIndex - 1);
                         out.println(command);
                         out.println(selectedConnection);
                     } else {
@@ -76,14 +75,34 @@ public class CommandClient {
                 } else if ("close".equalsIgnoreCase(command)) {
                     out.println(command);
                 }
-
-                String response;
-                while (!(response = in.readLine()).isEmpty()) {
-                    System.out.println("Risposta dal server: " + response);
-                }
             }
         } catch (Exception e) {
             System.out.println("Errore durante l'invio dei comandi: " + e.getMessage());
+        }
+    }
+
+    private static class ServerListener implements Runnable {
+        private final BufferedReader in;
+
+        ServerListener(BufferedReader in) {
+            this.in = in;
+        }
+
+        @Override
+        public void run() {
+            try {
+                String response;
+                while ((response = in.readLine()) != null) {
+                    System.out.println("Risposta dal router: " + response);
+                    if (response.startsWith("Connessione stabilita con il server finale ")) {
+                        availableConnections.add(response.substring("Connessione stabilita con il server finale ".length()));
+                    } else if (response.startsWith("Connessione chiusa con il server finale ")) {
+                        availableConnections.remove(response.substring("Connessione chiusa con il server finale ".length()));
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("Errore durante la lettura dalla connessione: " + e.getMessage());
+            }
         }
     }
 }
--- ESConnectionRouter.java
+++ ESConnectionRouter.java
@@ -5,143 +5,75 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class ESConnectionRouter {
-    private static final int COMMAND_PORT = 4321;
+    private static final int SERVER_PORT = 4321;
 
-    private final Map<String, Connection> connections = new HashMap<>();
-    private final ExecutorService executorService = Executors.newCachedThreadPool();
+    private static final Map<String, Socket> connections = new HashMap<>();
 
     public static void main(String[] args) {
-        new ESConnectionRouter().run();
-    }
-
-    private void run() {
-        try (ServerSocket serverSocket = new ServerSocket(COMMAND_PORT)) {
-            System.out.println("Server di comando in ascolto sulla porta " + COMMAND_PORT);
-
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
             while (true) {
-                Socket commandSocket = serverSocket.accept();
-                executorService.execute(new CommandHandler(commandSocket));
+                Socket clientSocket = serverSocket.accept();
+                new Thread(new ClientHandler(clientSocket)).start();
             }
         } catch (Exception e) {
-            System.out.println("Errore del server di comando: " + e.getMessage());
-        } finally {
-            executorService.shutdown();
+            System.out.println("Errore durante l'avvio del server: " + e.getMessage());
         }
     }
 
-    private class CommandHandler implements Runnable {
-        private final Socket commandSocket;
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
 
-        CommandHandler(Socket commandSocket) {
-            this.commandSocket = commandSocket;
+        ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
         }
 
         @Override
         public void run() {
             try (
-                BufferedReader in = new BufferedReader(new InputStreamReader(commandSocket.getInputStream()));
-                PrintWriter out = new PrintWriter(commandSocket.getOutputStream(), true)
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
             ) {
-                System.out.println("Connessione stabilita con il client " + commandSocket.getRemoteSocketAddress());
-
-                String command = in.readLine();
-                String[] parts = command.split(" ", 2);
-
-                if ("quit".equalsIgnoreCase(parts[0])) {
-                    String ipAddress = in.readLine();
-                    int port = Integer.parseInt(in.readLine());
-                    String key = ipAddress + ":" + port;
-                    Connection conn = connections.get(key);
-                    if (conn != null) {
-                        conn.closeConnection();
-                        connections.remove(key);
-                        System.out.println("Connessione chiusa con il server finale " + key);
-                    } else {
-                        System.out.println("Connessione non trovata.");
-                    }
-                } else if ("close".equalsIgnoreCase(parts[0])) {
-                    for (Connection conn : connections.values()) {
-                        conn.closeConnection();
-                    }
-                    connections.clear();
-                    System.out.println("Tutte le connessioni sono state chiuse.");
-                } else if ("connect".equalsIgnoreCase(parts[0])) {
-                    String ipAddress = in.readLine();
-                    int port = Integer.parseInt(in.readLine());
-                    String key = ipAddress + ":" + port;
-                    if (!connections.containsKey(key)) {
-                        try {
-                            Connection conn = new Connection(ipAddress, port);
-                            connections.put(key, conn);
-                            executorService.execute(conn);
-                            System.out.println("Connessione stabilita con il server finale " + key);
-                        } catch (Exception e) {
-                            System.out.println("Errore durante la creazione della connessione: " + e.getMessage());
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    if ("connect".equalsIgnoreCase(inputLine)) {
+                        String ipAddress = in.readLine();
+                        int port = Integer.parseInt(in.readLine());
+                        Socket echoServerSocket = new Socket(ipAddress, port);
+                        connections.put(ipAddress + ":" + port, echoServerSocket);
+                        out.println("Connessione stabilita con il server finale " + ipAddress + ":" + port);
+                    } else if ("list".equalsIgnoreCase(inputLine)) {
+                        for (String connection : connections.keySet()) {
+                            out.println(connection);
                         }
-                    }
-                } else if ("send".equalsIgnoreCase(parts[0])) {
-                    String ipAddress = in.readLine();
-                    int port = Integer.parseInt(in.readLine());
-                    String key = ipAddress + ":" + port;
-                    Connection conn = connections.get(key);
-                    if (conn != null) {
+                    } else if ("send".equalsIgnoreCase(inputLine)) {
+                        String connectionName = in.readLine();
                         String message = in.readLine();
-                        conn.sendMessage(message);
-                        System.out.println("Messaggio inviato al server finale " + key + ": " + message);
-                    } else {
-                        System.out.println("Connessione non trovata.");
+                        Socket echoServerSocket = connections.get(connectionName);
+                        if (echoServerSocket != null) {
+                            PrintWriter echoServerOut = new PrintWriter(echoServerSocket.getOutputStream(), true);
+                            echoServerOut.println(message);
+                            BufferedReader echoServerIn = new BufferedReader(new InputStreamReader(echoServerSocket.getInputStream()));
+                            out.println("Risposta dal server finale: " + echoServerIn.readLine());
+                        }
+                    } else if ("quit".equalsIgnoreCase(inputLine)) {
+                        String connectionName = in.readLine();
+                        Socket echoServerSocket = connections.remove(connectionName);
+                        if (echoServerSocket != null) {
+                            echoServerSocket.close();
+                            out.println("Connessione chiusa con il server finale " + connectionName);
+                        }
+                    } else if ("close".equalsIgnoreCase(inputLine)) {
+                        clientSocket.close();
+                        for (Socket echoServerSocket : connections.values()) {
+                            echoServerSocket.close();
+                        }
+                        connections.clear();
                     }
                 }
-                out.println(""); // Send an empty line to signal the end of the response.
             } catch (Exception e) {
-                System.out.println("Errore del server di comando: " + e.getMessage());
-            } finally {
-                try {
-                    commandSocket.close();
-                } catch (Exception e) {
-                    System.out.println("Errore durante la chiusura della connessione: " + e.getMessage());
-                }
-            }
-        }
-    }
-
-    private static class Connection implements Runnable {
-        private final Socket socket;
-        private final BufferedReader in;
-        private final PrintWriter out;
-
-        Connection(String ip, int port) throws Exception {
-            socket = new Socket(ip, port);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(socket.getOutputStream(), true);
-        }
-
-        @Override
-        public void run() {
-            try {
-                String response;
-                while ((response = in.readLine()) != null) {
-                    System.out.println("Risposta dal server finale: " + response);
-                }
-            } catch (Exception e) {
-                System.out.println("Errore durante la lettura dalla connessione: " + e.getMessage());
-            }
-        }
-
-        void sendMessage(String message) {
-            out.println(message);
-        }
-
-        void closeConnection() {
-            out.println("QUIT");
-            try {
-                socket.close();
-            } catch (Exception e) {
-                System.out.println("Errore durante la chiusura della connessione: " + e.getMessage());
+                System.out.println("Errore durante la gestione del client: " + e.getMessage());
             }
         }
     }
